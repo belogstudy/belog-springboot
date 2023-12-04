e6a0eae912d0e787349a4889e81ae023e8ec4d17
DiligentP <qkrwjdgus0603@gmail.com>
2023-12-04 23:46:10 +0900

    fix : htpps 에서 API 요청이 안되는 문제 수정

--- src/main/java/com/velog/velogproject/VelogProjectApplication.java
+++ src/main/java/com/velog/velogproject/VelogProjectApplication.java
@@ -1,9 +1,15 @@
 package com.velog.velogproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {
+		@Server(url = "https://belog.diligentp.com", description = "Belog Server")
+})
 public class VelogProjectApplication {
 
 	public static void main(String[] args) {
