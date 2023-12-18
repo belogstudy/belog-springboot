d2aaf4993febb323db453a579b1d0df9dafe4f68
DiligentP <qkrwjdgus0603@gmail.com>
2023-12-18 14:48:07 +0900

    fix: deploy.sh 에서 컨테이너 배포가 무시되는 오류 수정

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-# git 설치 확인 및 설치
+# 의존성 패키지 확인
 if ! command -v git &> /dev/null; then
-    echo "git이 설치되어 있지 않습니다. git을 설치합니다."
-    sudo yum install -y git
+    echo "git이 설치되어 있지 않습니다."
+    exit 1
 fi
 
 # JRE 17 설치 확인 및 설치
 if ! command -v java &> /dev/null; then
-    echo "Java 17 (JRE)이 설치되어 있지 않습니다. Java 17을 설치합니다."
-    sudo yum install -y java-17-openjdk
+    echo "Java 17 (JRE)이 설치되어 있지 않습니다."
+    exit 1
 fi
 
 project_dir="belog-springboot"
 store_url="https://github.com/belogstudy/belog-springboot.git"
+submodule_url="https://$ACTION_TOKEN@github.com/belogstudy/belog-springboot-secret.git"
 dockerfile_path="$HOME/$project_dir/Dockerfile"
 
 # 프로젝트 가져오기
@@ -28,6 +29,15 @@ else
     git clone "$store_url" || exit 1
     cd "$project_dir" || exit 1  # 클론 후 프로젝트 폴더로 이동
 fi
+
+# 서브모듈 가져오기
+echo "설정파일을 변경사항을 확인중입니다."
+if [ -d "$project_dir/belog-springboot-secret" ]; then
+    echo "설정 파일을 업데이트합니다."
+    rm -rf "$project_dir/belog-springboot-secret"
+fi
+git clone "$submodule_url"
+mv belog-springboot-secret/application.yml "$project_dir/belog-springboot-secret"
 
 # 프로젝트 빌드
 echo "프로젝트를 빌드합니다."
@@ -47,6 +57,8 @@ echo "이전 이미지를 삭제합니다."
 docker rmi velog-spring-app-image || true  # 이미지가 없을 경우 에러를 무시
 
 # 새로운 이미지를 빌드하고 컨테이너를 실행합니다.
-docker build -t velog-spring-app-image -f "$dockerfile_path" "$HOME/$project_dir"  # Dockerfile의 경로를 홈 디렉토리 내 프로젝트 경로로 수정
+echo "도커 이미지를 빌드합니다."
+docker build -t velog-spring-app-image -f "$dockerfile_path" "$HOME/$project_dir" || { echo "도커 이미지 빌드에 실패했습니다."; exit 1; }
+echo "도커 컨테이너를 실행합니다."
 docker run --name velog-spring-app -p 80:8080 -d velog-spring-app-image
 
